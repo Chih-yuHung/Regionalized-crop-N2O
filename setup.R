59fa4c79c729b35d77a5ef6f52224500da28c9af
Hung <Chih-Yu.Hung@ec.gc.ca>
2025-07-30 16:11:08 -0400

    Major revision to produce GrainAgg and GrainSplit results.

--- setup.R
+++ setup.R
@@ -11,6 +11,8 @@ suppressPackageStartupMessages({
   library(ggrepel)
   library(scales)
   library(cowplot)     # for draw_plot() – installs with ggplot2 3.4+
+  library(furrr)
+  library(future)
 })
 
 
@@ -22,11 +24,17 @@ target_crops <- c("Rye(spring)", "Barley", "Wheat(spring)", "Corn(grain)", "Cano
 target_cropID <-  c(3, 4, 11, 14, 15, 16, 18, 19, 20, 21, 22, 24)
 
 # Define the mapping rules
-crop_mapping <- function(crop, class) {
+crop_mapping <- function(crop, class, UTIL_PRACTICE_DESC = NULL) {
+  
+  # If UTIL_PRACTICE_DESC is missing, replace with empty string
+  if (is.null(UTIL_PRACTICE_DESC)) {
+    UTIL_PRACTICE_DESC <- rep("", length(crop))
+  }
+  
   case_when(
     crop == "BARLEY" ~ "Barley",
-    crop == "CORN" & class == "GRAIN" ~ "Corn(grain)",
-    crop == "CORN" & class == "SILAGE" ~ "Corn(silage)",
+    crop == "CORN" & UTIL_PRACTICE_DESC == "GRAIN" ~ "Corn(grain)",
+    crop == "CORN" & UTIL_PRACTICE_DESC == "SILAGE" ~ "Corn(silage)",
     crop == "CORN" & class == "ALL CLASSES" ~ "Corn",
     crop == "RYE" & class == "SPRING" ~ "Rye(spring)",
     crop == "RYE" & class == "FALL" ~ "Rye(fall)",
@@ -35,7 +43,9 @@ crop_mapping <- function(crop, class) {
     crop == "WHEAT" & class == "SPRING, (EXCL DURUM)" ~ "Wheat(spring)",
     crop == "WHEAT" & class == "SPRING, DURUM" ~ "Wheat(durum)",
     crop == "SOYBEANS" ~ "Soybeans",
-    crop == "SORGHUM" ~ "Sorghum",
+    crop == "SORGHUM" & UTIL_PRACTICE_DESC == "GRAIN" ~ "Sorghum(grain)",
+    crop == "SORGHUM" & UTIL_PRACTICE_DESC == "SILAGE" ~ "Sorghum(silage)",
+    crop == "SORGHUM" & class == "ALL CLASSES" ~ "Sorghum",
     crop == "PEAS" ~ "Peas",
     crop == "TRITICALE" ~ "Triticale",
     crop == "CANOLA" ~ "Canola",
